7c0b590b854041a9b48cefc9115b7f5905f84300
Roque Rueda <skaroque@gmail.com>
2017-10-08 18:06:34 -0500

    Initial commit

---
+++ app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
@@ -0,0 +1,13 @@
+package imok.rueda.roque.com.imokhclmx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
