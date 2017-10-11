5a9f076b7ed09fde8db5fd79bd6a65f6441e990e
Roque Rueda <skaroque@gmail.com>
2017-10-11 05:46:37 -0500

    Create Contact List Fragment

--- app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
+++ app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
@@ -1,11 +1,14 @@
 package imok.rueda.roque.com.imokhclmx;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import imok.rueda.roque.com.imokhclmx.fragments.ContactListFragment;
 import imok.rueda.roque.com.imokhclmx.model.Contact;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_single_fragment);
 
-    mDatabase = FirebaseDatabase.getInstance().getReference();
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+    if (fragment == null) {
+      // No fragment!, add the list
+      fragment = new ContactListFragment();
+      fm.beginTransaction()
+              .add(R.id.fragment_container, fragment)
+              .commit();
+    }
+
+    /*mDatabase = FirebaseDatabase.getInstance().getReference();
     mContactReference = mDatabase.child(Contact.CONTACTS_REFERENCE);
 
-    mContactReference.setValue("Hello World");
+    mContactReference.setValue("Hello World");*/
   }
 }
