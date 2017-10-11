c32a67ddc8719921add1db03c1356baa5130d155
Roque Rueda <skaroque@gmail.com>
2017-10-11 05:28:00 -0500

    Firebase test! Happy this thing works like a charm, don't do this in production

--- app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
+++ app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
@@ -3,11 +3,22 @@ package imok.rueda.roque.com.imokhclmx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+  private DatabaseReference mDatabase;
+  private DatabaseReference mContactReference;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mDatabase = FirebaseDatabase.getInstance().getReference();
+    mContactReference = mDatabase.child("contacs");
+
+    mContactReference.setValue("Hello World");
   }
 }
