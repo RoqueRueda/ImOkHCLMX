b0f553ffb2c4b3bf76bcd3a99897acddd6aacf2e
Roque Rueda <skaroque@gmail.com>
2017-10-11 05:39:04 -0500

    Change on Activity to host fragments

--- app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
+++ app/src/main/java/imok/rueda/roque/com/imokhclmx/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_single_fragment);
 
     mDatabase = FirebaseDatabase.getInstance().getReference();
     mContactReference = mDatabase.child(Contact.CONTACTS_REFERENCE);
