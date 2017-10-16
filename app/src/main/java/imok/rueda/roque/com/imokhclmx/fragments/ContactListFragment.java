49560d89f38cd612f7d7bf0ed7b58f006d2a86da
Roque Rueda <skaroque@gmail.com>
2017-10-16 00:52:03 -0500

    Adding display on the contact list fragment

--- app/src/main/java/imok/rueda/roque/com/imokhclmx/fragments/ContactListFragment.java
+++ app/src/main/java/imok/rueda/roque/com/imokhclmx/fragments/ContactListFragment.java
@@ -2,12 +2,23 @@ package imok.rueda.roque.com.imokhclmx.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import imok.rueda.roque.com.imokhclmx.R;
+import imok.rueda.roque.com.imokhclmx.model.Contact;
 
 /**
  * List of contact that will be de main point of the app.
@@ -18,9 +29,34 @@ import imok.rueda.roque.com.imokhclmx.R;
  */
 public class ContactListFragment extends Fragment {
 
+  private DatabaseReference mDatabase;
+  private DatabaseReference mContactReference;
+  private List<Contact> mContacts;
+
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    mContacts = new ArrayList<>();
+
+    mDatabase = FirebaseDatabase.getInstance().getReference();
+    mContactReference = mDatabase.child(Contact.CONTACTS_REFERENCE);
+
+    mContactReference.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        for (DataSnapshot contactSnapShoot : dataSnapshot.getChildren()) {
+          Contact contact = contactSnapShoot.getValue(Contact.class);
+          mContacts.add(contact);
+        }
+      }
+
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+        View fragmentContainer = getActivity().findViewById(R.id.fragment_container);
+        Snackbar.make(fragmentContainer, getText(R.string.err_msg_contact_problem),
+                Snackbar.LENGTH_SHORT);
+      }
+    });
   }
 
   @Nullable
